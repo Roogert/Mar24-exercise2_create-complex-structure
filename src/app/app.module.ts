2b7affcae29db1050c8451261139719bb4f08d15
Roogert <samrellergert@gmail.com>
2022-03-25 23:07:19 -0500

    files for exercise 2 & 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FriendsListComponent } from './friends-list/friends-list.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    PostsComponent,
+    FriendsComponent,
+    FriendsListComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule
